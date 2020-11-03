1cb172252fffbe7e0795f46606be2980ac42fa59
Aleah Fredrickson <aleah.fredrickson@gmail.com>
2020-11-03 10:28:50 -0700

    Remove 'open popup' button

--- ghost-cat-app/src/app/View/app.component.ts
+++ ghost-cat-app/src/app/View/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ModalComponent } from './modal/modal.component';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,17 +6,4 @@ import { ModalComponent } from './modal/modal.component';
 })
 export class AppComponent {
   title = 'ghost-cat-app';
-
-  constructor(public dialog: MatDialog) { }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ModalComponent, {
-      minWidth: '800px',
-      data: {
-        url: "https://www.ngi.org.uk/wp-content/uploads/2016/08/The-four-otters-from-left-Ash-Tod-Pip-and-Sam-Copyright-Ian-Greneholt.jpg",
-        metadataList: ["Camera Trap Name: site002", "Date: 01/01/2020", "Camera make: CUDDEBACK", "Camera model: Ambush"],
-        animalList: ["Mule deer: 83%", "Cow: 0.1%"]
-      }
-    });
-  }
 }
