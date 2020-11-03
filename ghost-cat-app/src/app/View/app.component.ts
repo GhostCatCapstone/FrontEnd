5df34965c2627f58b629dc8514f96304cf32c0f9
Aleah Fredrickson <aleah.fredrickson@gmail.com>
2020-11-02 23:03:25 -0700

    Add component for image popup

--- ghost-cat-app/src/app/View/app.component.ts
+++ ghost-cat-app/src/app/View/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ghost-cat-app';
+
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      minWidth: '800px',
+      data: {
+        url: "https://www.ngi.org.uk/wp-content/uploads/2016/08/The-four-otters-from-left-Ash-Tod-Pip-and-Sam-Copyright-Ian-Greneholt.jpg",
+        metadataList: ["Camera Trap Name: site002", "Date: 01/01/2020", "Camera make: CUDDEBACK", "Camera model: Ambush"],
+        animalList: ["Mule deer: 83%", "Cow: 0.1%"]
+      }
+    });
+  }
 }
--- ghost-cat-app/src/app/View/app.module.ts
+++ ghost-cat-app/src/app/View/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SendrequesttoserverComponent } from './sendrequesttoserver/sendrequesttoserver.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, SendrequesttoserverComponent],
+  declarations: [AppComponent, SendrequesttoserverComponent, ModalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -18,8 +21,11 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     MatDividerModule,
     NoopAnimationsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [ModalComponent],
 })
-export class AppModule {}
+export class AppModule { }
