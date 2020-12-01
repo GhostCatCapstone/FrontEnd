aa6bf86c8e4d0527414c89e12878be91caea73a1
jeffersonbarney <jefferson.barney@icloud.com>
2020-11-30 18:17:50 -0700

    Connects the gallery to data fetched from the backend

---
+++ ghost-cat-app/src/app/Model/ImageQueryRequest.ts
@@ -0,0 +1,14 @@
+import { ClassValue } from './ClassValue';
+
+export class ImageQueryRequest {
+  constructor(
+    public userID: string,
+    public authToken: string,
+    public projectID: string,
+    public minDate?: number,
+    public maxDate?: number,
+    public deployment?: string,
+    public cameraTrap?: string,
+    public classes?: ClassValue[]
+  ) {}
+}
