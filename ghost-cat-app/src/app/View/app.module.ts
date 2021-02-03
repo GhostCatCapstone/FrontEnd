d75c4f11f567a6f0fc1703fd9411b72fe8d6054f
jeffersonbarney <jefferson.barney@icloud.com>
2021-02-03 15:17:33 -0700

    Adds navigation to map view page from Search

--- ghost-cat-app/src/app/View/app.module.ts
+++ ghost-cat-app/src/app/View/app.module.ts
@@ -1,14 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
@@ -17,7 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { GalleryModule } from 'ng-gallery';
-import { GALLERY_CONFIG } from 'ng-gallery';
 import { ImageDetailsComponent } from './image-details/image-details.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
@@ -27,6 +18,7 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { InternalRegisterPageComponent } from './internal-register-page/internal-register-page.component';
 import { ImageThumbnailsPageComponent } from './image-thumbnails-page/image-thumbnails-page.component';
+import { MapViewPageComponent } from './map-view-page/map-view-page.component';
 import { GhostCatLoginComponent } from './ghost-cat-login/ghost-cat-login.component';
 import { ExpandableListModule } from 'angular-expandable-list';
 
@@ -43,19 +35,12 @@ import { ExpandableListModule } from 'angular-expandable-list';
     RegisterPageComponent,
     AccountSettingsComponent,
     InternalRegisterPageComponent,
+    MapViewPageComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
     NoopAnimationsModule,
     HttpClientModule,
     BrowserAnimationsModule,
@@ -63,17 +48,7 @@ import { ExpandableListModule } from 'angular-expandable-list';
     GalleryModule,
     ExpandableListModule,
   ],
-  providers: [
-    {
-      provide: GALLERY_CONFIG,
-      useValue: {
-        dots: false,
-        imageSize: 'cover',
-      },
-    },
-    MatDatepickerModule,
-  ],
   bootstrap: [AppComponent],
   entryComponents: [],
 })
-export class AppModule { }
+export class AppModule {}
